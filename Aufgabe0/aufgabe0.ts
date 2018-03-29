e94a3ab7a0e843b5e60c282237fd1aa11a028ff2
sophie <sophie@sophies-mbp.eduroam.hs-furtwangen.de>
2018-03-29 12:34:46 +0200

    Verbesserung aufgabe 0: Typ-Annotation

--- Aufgabe0/aufgabe0.ts
+++ Aufgabe0/aufgabe0.ts
@@ -2,8 +2,8 @@
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde
 nicht kopiert und auch nicht diktiert. */
 
-function Begrüßung() {
-  var name = prompt("Bitte geben Sie Ihren Namen ein", "");
+function Begrüßung(): void {
+  var name: string = prompt("Bitte geben Sie Ihren Namen ein", "");
   if (name != null) {
     document.getElementById("Type").innerHTML =
       "Hallo " + name + "! Herzlich Willkommen auf der Mini-Website.";
