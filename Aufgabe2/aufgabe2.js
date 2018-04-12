16eed6fd3e17c12411781d7b4b2afe84fe8eb2f8
sophie <sophie@sophies-mbp.hfu-open.fu.hs-furtwangen.de>
2018-04-12 12:11:45 +0200

    Konzept aufgabe2

---
+++ Aufgabe2/aufgabe2.js
@@ -0,0 +1,4 @@
+/* Aufgabe: 1 - Boxes Name: Sopehi Köperl Matrikel:257762 Datum: 31.03.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. */ 
+//# sourceMappingURL=aufgabe2.js.map
---
+++ Aufgabe2/aufgabe2.ts
@@ -0,0 +1,3 @@
+/* Aufgabe: 1 - Boxes Name: Sopehi Köperl Matrikel:257762 Datum: 31.03.2018 
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. */
