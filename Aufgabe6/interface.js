9e9ce8a923ea0770a1c36105f51cb2cd5907334a
sophie <sophie@sophies-mbp-2.fritz.box>
2018-05-27 14:49:37 +0200

    A6

---
+++ Aufgabe6/interface.js
@@ -0,0 +1,6 @@
+var Aufgabe6;
+(function (Aufgabe6) {
+    // Homogen assoziativ: Speicherung unter Matrikelnummer
+    Aufgabe6.studiHomoAssoc = {};
+})(Aufgabe6 || (Aufgabe6 = {}));
+//# sourceMappingURL=interface.js.map
---
+++ Aufgabe6/interface.ts
@@ -0,0 +1,19 @@
+namespace Aufgabe6{
+    // heteroge assoziativ: Datensatz
+    export interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        age: number;
+        gender: boolean;
+        course: string;
+    }
+
+     // homogen assoziativ: Datensatz wird Matrikelnummer zugeordnet
+    export interface Studis {
+         [matrikel: string]: Studi;
+    }
+ 
+    // Homogen assoziativ: Speicherung unter Matrikelnummer
+    export let studiHomoAssoc: Studis = {};  
+}
