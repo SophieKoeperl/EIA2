6527560496b442749aaf2a79954f6bc032636c30
sophie <sophie@sophies-mbp-2.hfu-open.fu.hs-furtwangen.de>
2018-06-17 14:50:40 +0200

    9 fertig :)

--- Aufgabe8/interface.js
+++
@@ -1,6 +0,0 @@
-var Aufgabe8;
-(function (Aufgabe8) {
-    // Homogen assoziativ: Speicherung unter Matrikelnummer
-    Aufgabe8.studiHomoAssoc = {};
-})(Aufgabe8 || (Aufgabe8 = {}));
-//# sourceMappingURL=interface.js.map
